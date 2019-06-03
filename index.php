2dc7a80c89f6f463353d9cd71cb87c9a527d88ce
sapcprogrammer <sapc.programmer@gmail.com>
2019-06-03 16:47:46 +0800

    additional changes

--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 echo "test";
 echo "Commits - Use git add -p to interactively stage specific portions of the modified files.";
 echo "Commits - Commit early and often";
+
+echo "Commits should be ordered logically";
